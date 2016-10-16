be1c4883c6d249cef67af1ee474aa2b34d7a155a
Octogonapus <firey45@gmail.com>
2016-10-15 23:12:44 -0400

    Added ignored str param to bci sem

--- Modules/Core/util.h
+++ Modules/Core/util.h
@@ -54,6 +54,8 @@ int bci_sem_debug_full_counter = 0;
 #define BCI_lockSem(sem, str) bci_sem_debug_full_counter++;if(bci_sem_debug_full_counter != 1){writeDebugStreamLine("BCI_lockSem: overlock at %d", str);}else{writeDebugStreamLine("BCI_lockSem: %d",str);}semaphoreLock(sem);if(bDoesTaskOwnSemaphore(sem))
 #define BCI_unlockSem(sem, str) bci_sem_debug_full_counter--;if(bci_sem_debug_full_counter != 0){writeDebugStreamLine("BCI_unlockSem: underlock at %d", str);}else{writeDebugStreamLine("BCI_unlockSem: %d",str);}if(bDoesTaskOwnSemaphore(sem)){semaphoreUnlock(sem);}
 #else
+#define BCI_lockSem(sem, str) semaphoreLock(sem);if(bDoesTaskOwnSemaphore(sem))
+#define BCI_unlockSem(sem, str) if(bDoesTaskOwnSemaphore(sem)){semaphoreUnlock(sem);}
 #define BCI_lockSem(sem) semaphoreLock(sem);if(bDoesTaskOwnSemaphore(sem))
 #define BCI_unlockSem(sem) if(bDoesTaskOwnSemaphore(sem)){semaphoreUnlock(sem);}
 #endif //BCI_SEM_DEBUG
