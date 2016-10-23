adb4013e5a334e9673ea18dbd4b91666a9ea6e80
Octogonapus <firey45@gmail.com>
2016-10-23 16:28:32 -0400

    Added slew rate comments

--- Modules/Core/motorControl.h
+++ Modules/Core/motorControl.h
@@ -6,23 +6,71 @@
 //Motor general
 #define MOTOR_NUM               10     //Used to be kNumbOfTotalMotors, changed for now because
                                        //that could result in a bug where a motor is not accessible
-#define MOTOR_MIN_VALUE         (-127) //Minimum power
-#define MOTOR_MAX_VALUE         127    //Maximum power
+#define MOTOR_MIN_VALUE         (-127) //Minimum speed
+#define MOTOR_MAX_VALUE         127    //Maximum speed
 #define MOTOR_DEFAULT_SLEW_RATE 10     //Feels like nearly no slewing to a driver
 #define MOTOR_FAST_SLEW_RATE    256    //No slewing in output
-#define MOTOR_TASK_DELAY        15     //Wait 15ms between batch motor power updates
+#define MOTOR_TASK_DELAY        15     //Wait 15ms between batch motor speed updates
+/**
+ * Sets the speed of a motor
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @param  speed New speed
+ */
+#define setMotorSpeed(index, speed) driveMotors[index].reqSpeed = (speed)
 
-//Motor control
-#define setMotorSpeed(index, power) driveMotors[index].reqSpeed = (power)
-#define setMotorSpeed_Bypass(index, power) driveMotors[index].reqSpeed = (power); driveMotors[index].artSpeed = (power)
+/**
+ * Sets the powr of a motor instantly (bypassing any slewing)
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @param  speed New speed
+ */
+#define setMotorSpeed_Bypass(index, speed) driveMotors[index].reqSpeed = (speed); driveMotors[index].artSpeed = (speed)
+
+/**
+ * Sets the slew rate of a motor
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @param  rate  New slew rate
+ */
 #define setMotorSlew(index, rate) driveMotors[index].slew = (rate)
+
+/**
+ * Returns the driveMotor representation of a motor
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @return       driveMotor representation of the motor
+ */
 #define getMotor(index) driveMotors[index]
+
+/**
+ * Returns the motor speed of a motor (target speed, not current speed)
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @return       Motor speed
+ */
 #define getMotorSpeed(index) driveMotors[index].reqSpeed
+
+/**
+ * Enables slewing of a motor (slewing enabled by default)
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ */
 #define setMotorActive(index) driveMotors[index].active = true
+
+/**
+ * Disables slewing of a motor
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ */
 #define setMotorInactive(index) driveMotors[index].active = false
 
-//Raw motor control
-#define setMotorSpeedRaw(index, power) motor[index] = (power)
+/**
+ * Sets the raw speed of a motor. The slew rate controller will fight this value
+ * unless the motor has been disabled with setMotorInactive()
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @param  speed New speed
+ */
+#define setMotorSpeedRaw(index, speed) motor[index] = (speed)
+
+/**
+ * Returns the speed of a motor (true current speed, not target speed)
+ * @param  index Motor to use (as named in Motors and Sensors Setup)
+ * @return       Motor speed
+ */
 #define getMotorSpeedRaw(index) motor[index]
 
 //Extra motor control
@@ -37,10 +85,17 @@ typedef struct driveMotor_t
 	bool active;    //Whether or not to update this motor
 } driveMotor;
 
-//Adds and initializes a motor (by name) to the motor array
+/**
+ * Initializes a driveMotor
+ * @param  name     Motor to use (as named in Motor and Sensors Setup)
+ * @param  slewRate Slew rate
+ * @return          Initialized driveMotor
+ */
 driveMotor* addMotor(const tMotor name, float slewRate = MOTOR_DEFAULT_SLEW_RATE);
 
-//Updates the power of each motor to best meet the requested power
+/**
+ * Loops through all driveMotors, slewing active ones to meet their target
+ */
 task motorSlewRateTask();
 
 #endif //BCI_MOTORCONTROL_H_INCLUDED
