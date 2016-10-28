b4460a5462edf79cc7c2b785c15170a9a38f3a94
Octogonapus <firey45@gmail.com>
2016-10-28 12:49:31 -0400

    Added header and malloc

--- Modules/Heap/heap.c
+++ Modules/Heap/heap.c
@@ -3,14 +3,95 @@
 
 #include "heap.h"
 
+//Using BNS Error code here as a flag
+//https://github.com/JMarple/BNSLibrary/blob/master/Modules/Core/BNSHeap.h
+#define BCI_FREE_FLAG 2852126720
+
 static float bciHeap[BCI_HEAP_SIZE];
-static string bciHeap_Names[25];
+
+//Float to reference for a malloc fail
+float BCI_MALLOC_FAIL = 0;
 
 void heap_init()
 {
-  
+  for (int i = 0; i < BCI_HEAP_SIZE; i++)
+  {
+    bciHeap[i] = BCI_DATA_FLAG;
+  }
 }
 
-bool heap_malloc(const string *name, const int type, void *data);
+float* heap_malloc(const unsigned int size, bool recursive)
+{
+  //Get next free spot in heap
+  unsigned int i = 0, runLength = 0;
+  while (true)
+  {
+    //Empty flag
+    if (bciHeap[i] == BCI_FREE_FLAG)
+    {
+      runLength++;
+    }
+    else
+    {
+      runLength = 0;
+    }
+
+    //If block is long enough, use it
+    if (runLength == size)
+    {
+      //Clear free flags
+      for (int j = runLength; j > 0; j--)
+      {
+        bciHeap[i - j] = 0;
+      }
+
+      return &(bciHeap[i]);
+    }
+
+    //If we've gone down the entire heap
+    if (i == BCI_HEAP_SIZE - 1)
+    {
+      //If we've already tried to defrag
+      if (recursive)
+      {
+        return &BCI_MALLOC_FAIL;
+      }
+      else
+      {
+        //Defragment the heap
+
+        //Try again
+        return heap_malloc(size, true);
+      }
+    }
+
+    i++;
+  }
+}
+
+matrix* heap_matrix_malloc(const unsigned int width, const unsigned int height)
+{
+
+}
+
+void heap_free(const unsigned int loc, const unsigned int size)
+{
+
+}
+
+void heap_free_matrix(matrix *mat)
+{
+
+}
+
+void heap_print(const unsigned int loc, const unsigned int size)
+{
+
+}
+
+void heap_printStats(const unsigned int loc, const unsigned int size)
+{
+
+}
 
 #endif //BCI_HEAP_C_INCLUDED
--- Modules/Heap/heap.h
+++ Modules/Heap/heap.h
@@ -5,18 +5,59 @@
 #define BCI_HEAP_SIZE 1000
 #endif //BCI_HEAP_SIZE
 
+typedef stuct matrix_t
+{
+  float width;
+  float height;
+  float *data;
+} matrix;
+
 /**
  * Initializes the heap
  */
 void heap_init();
 
 /**
- * Allocates memory for a type of object
- * @param  name Name of object
- * @param  type Type of object
- * @param  data Data in object
- * @return      Whether the operation was successful
+ * Allocates memory on the heap
+ * @param  size      Number of floats to allocate
+ * @param  recursive Internal parameter - Do not use
+ * @return           Memory reference
  */
-bool heap_malloc(const string *name, const int type, void *data);
+float* heap_malloc(const unsigned int size, bool recursive = false);
+
+/**
+ * Allocates a matrix on the heap
+ * @param  width  Width of matrix
+ * @param  height Height of matrix
+ * @return        Matrix reference
+ */
+matrix* heap_matrix_malloc(const unsigned int width, const unsigned int height);
+
+/**
+ * Frees memory
+ * @param loc  Location to free from
+ * @param size Length to free
+ */
+void heap_free(const unsigned int loc, const unsigned int size);
+
+/**
+ * Frees a matrix
+ * @param mat Matrix to free
+ */
+void heap_free_matrix(matrix *mat);
+
+/**
+ * Prints a range of the heap
+ * @param loc  Location to print from
+ * @param size Length to print
+ */
+void heap_print(const unsigned int loc, const unsigned int size);
+
+/**
+ * Prints diagnostics for a range of the heap
+ * @param loc  Location to print from
+ * @param size Length to print
+ */
+void heap_printStats(const unsigned int loc, const unsigned int size);
 
 #endif //BCI_HEAP_H_INCLUDED
