d79f4674674373ade9dc02daf2797220f1905f16
Octogonapus <firey45@gmail.com>
2016-11-05 01:56:56 -0400

    Added to dema

--- Modules/Filter/demaFilter.c
+++ Modules/Filter/demaFilter.c
@@ -3,28 +3,33 @@
 
 #include "demaFilter.h"
 
-void filter_Init_DEMA(DEMAFilter *filter)
+void filter_Init_DEMA(DEMAFilter *filter, float alpha, float beta)
 {
-	filter->alpha = 1.0;
-	filter->beta = 1.0;
-	filter->readIn = 0.0;
+	filter->alpha = alpha;
+	filter->beta = beta;
 	filter->outputS = 0.0;
 	filter->outputB = 0.0;
 	filter->outputS_old = 0.0;
 	filter->outputB_old = 0.0;
 }
 
-float filter_DEMA(DEMAFilter *filter, const float readIn, const float alpha, const float beta)
+float filter_DEMA(DEMAFilter *filter, const float readIn)
 {
 	filter->alpha = alpha;
 	filter->beta = beta;
-	filter->readIn = readIn;
 	filter->outputS = (alpha * readIn) + ((1.0 - alpha) * (filter->outputS_old + filter->outputB_old));
 	filter->outputB = (beta * (filter->outputS - filter->outputS_old)) + ((1.0 - beta) * filter->outputB_old);
 	filter->outputS_old = filter->outputS;
 	filter->outputB_old = filter->outputB;
 
 	return filter->outputS + filter->outputB;
+}
+
+float filter_DEMA(DEMAFilter *filter, const float readIn, const float alpha, const float beta)
+{
+	filter->alpha = alpha;
+	filter->beta = beta;
+	filter_DEMA(filter, readIn);
 }
 
 #endif //BCI_DEMAFILTER_C_INCLUDED
--- Modules/Filter/demaFilter.h
+++ Modules/Filter/demaFilter.h
@@ -6,7 +6,6 @@ typedef struct DEMAFilter_t
 {
 	float alpha;
 	float beta;
-	float readIn;
 	float outputS;
 	float outputB;
 	float outputS_old;
@@ -17,7 +16,15 @@ typedef struct DEMAFilter_t
  * Initializes a double exponential moving average filter
  * @param filter DEMA filter to initialize
  */
-void filter_Init_DEMA(DEMAFilter *filter);
+void filter_Init_DEMA(DEMAFilter *filter, float alpha = 1.0, float beta = 1.0);
+
+/**
+ * Filters an input
+ * @param  filter DEMA filter to use
+ * @param  readIn Input reading
+ * @return        Filtered value
+ */
+float filter_DEMA(DEMAFilter *filter, const float readIn);
 
 /**
  * Filters an input
