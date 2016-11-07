fcf8c1135e55c39514e4191803628a6c239d63f8
Octogonapus <firey45@gmail.com>
2016-11-07 03:12:21 -0500

    Implemented arrayList

--- Modules/Heap/arrayList.c
+++ Modules/Heap/arrayList.c
@@ -3,37 +3,107 @@
 
 #include "arrayList.h"
 
-bool arrayList_Initialize(arrayList *list, const int size)
+bool arrayList_Initialize(arrayList *list, const unsigned int size)
 {
-  list->size = size;
-  list->capacity = size;
+  list->usedSpace = 0;
+  return block_Initialize(&(list->b), size);
+}
 
-  if ((list->startIndex = heap_Malloc(size)) == BCI_HEAP_FAIL)
+bool arrayList_TrimToSize(arrayList *list)
+{
+  return block_Shrink(&(list->b), list->b.size - list->usedSpace);
+}
+
+bool arrayList_EnsureCapacity(arrayList *list, const unsigned int minCapacity)
+{
+  if (minCapacity > list->b.size)
   {
-    return false;
+    return block_Expand(&(list->b), minCapacity);
   }
 
   return true;
 }
 
-void arrayList_Initialize(arrayList *list)
+unsigned int arrayList_Size(arrayList *list)
 {
-  arrayList_Initialize(list, 1);
+  return list->b.size;
 }
 
-void arrayList_Add(arrayList *list, const float data)
+float arrayList_Get(const arrayList *list, const unsigned int index)
 {
-  //If we can just add the element
-  if (list->size + 1 <= list->capacity)
+  //Bounds check
+  if (index < 0 || index > list->b.size)
   {
-    heap_Set(list->size++, data);
+    #ifdef BCI_HEAP_DEBUG
+      writeDebugStreamLine("BCI HEAP ERROR: arrayList_Get: Invalid location: %d", index);
+    #endif
+
+    return BCI_HEAP_FAIL;
   }
-  //Else, we have to realloc
-  else
+
+  return block_Get(&(list->b), index);
+}
+
+bool arrayList_Set(arrayList *list, const unsigned int index, const float data)
+{
+  //Bounds check
+  if (index < 0 || index > list->b.size)
   {
-    heap_Expand(list->startIndex, list->capacity, 1);
-    //TODO: if expand fails, we need to defragment the heap
+    #ifdef BCI_HEAP_DEBUG
+      writeDebugStreamLine("BCI HEAP ERROR: arrayList_Set: Invalid location: %d", index);
+    #endif
+
+    return BCI_HEAP_FAIL;
   }
+
+  return block_Set(&(list->b), index, data);
+}
+
+bool arrayList_Add(arrayList *list, const float data)
+{
+  arrayList_EnsureCapacity(list, list->b.size + 1);
+  block_Set(&(list->b), list->usedSpace++, data);
+}
+
+float arrayList_Remove(arrayList *list, const unsigned int index)
+{
+  //Bounds check
+  if (index < 0 || index > list->b.size)
+  {
+    #ifdef BCI_HEAP_DEBUG
+      writeDebugStreamLine("BCI HEAP ERROR: arrayList_Set: Invalid location: %d", index);
+    #endif
+
+    return BCI_HEAP_FAIL;
+  }
+
+  float out = arrayList_Get(list, index);
+
+  //If index isn't the last element, we have to shift bottom elements up
+  if (index != list->b.size)
+  {
+    for (unsigned int i = index; i < list->b.size - 1; i++)
+    {
+      block_Set(&(list->b), index, block_Get(&(list->b), index + 1));
+    }
+  }
+
+  return out;
+}
+
+bool arrayList_Clear(arrayList *list, float clearVal)
+{
+  for (unsigned int i = 0; i < list->b.size; i++)
+  {
+    block_Set(&(list->b), i, clearVal);
+  }
+
+  return true;
+}
+
+bool arrayList_Free(arrayList *list)
+{
+  return block_Free(&(list->b));
 }
 
 #endif //BCI_ARRAYLIST_C_INCLUDED
--- Modules/Heap/arrayList.h
+++ Modules/Heap/arrayList.h
@@ -3,30 +3,87 @@
 
 typedef struct arrayList_t
 {
-  int size;     // Current size
-  int capacity; // Maximum capacity until another malloc
-  int startIndex;
+  block b;
+  unsigned int usedSpace;
 } arrayList;
 
 /**
- * Initializes an ArrayList
- * @param list ArrayList to initialze
+ * Initializes an arrayList
+ * @param list arrayList to initialze
  * @param size Starting size
- * @return     Whether the ArrayList was initialized successfully
+ * @return     Whether the arrayList was initialized successfully
  */
-bool arrayList_Initialize(arrayList *list, const int size);
+bool arrayList_Initialize(arrayList *list, const unsigned int size = 10);
 
 /**
- * Initializes an ArrayList
- * @param list ArrayList to initialze
+ * Trims the capacity of this arrayList down to the number of elements used
+ * @param  list arrayList to trim
+ * @return      Whether the trim was successful
  */
-void arrayList_Initialize(arrayList *list);
+bool arrayList_TrimToSize(arrayList *list);
 
 /**
- * Adds an element to an ArrayList
- * @param list ArrayList to use
- * @param data Elements to add
+ * Increases the capacity of this arrayList, if necessary, to ensure that it can
+ * hold at least the number of elements specified
+ * @param  list        arrayList to modify
+ * @param  minCapacity Minimum capacity
+ * @return             Whether the operation was successful
  */
-void arrayList_Add(arrayList *list, const float data);
+bool arrayList_EnsureCapacity(arrayList *list, const unsigned int minCapacity);
+
+/**
+ * Returns the size of this arrayList
+ * @param  list arrayList to read from
+ * @return      The size of the arrayList
+ */
+unsigned int arrayList_Size(arrayList *list);
+
+/**
+ * Returns the data at an index in this arrayList
+ * @param  list  arrayList to read from
+ * @param  index Index of data
+ * @return       Data at index
+ */
+float arrayList_Get(const arrayList *list, const unsigned int index);
+
+/**
+ * Sets the data at an index in this arrayList
+ * @param  list  arrayList to write to
+ * @param  index Index of data
+ * @param  data  Data to write
+ * @return       Whether the write was successful
+ */
+bool arrayList_Set(arrayList *list, const unsigned int index, const float data);
+
+/**
+ * Adds data to this arrayList
+ * @param  list arrayList to add to
+ * @param  data Data to add
+ * @return      Whether the add was successful
+ */
+bool arrayList_Add(arrayList *list, const float data);
+
+/**
+ * Removed an element from this arrayList
+ * @param  list  arrayList to remove from
+ * @param  index Index of data
+ * @return       Data removed
+ */
+float arrayList_Remove(arrayList *list, const unsigned int index);
+
+/**
+ * Clears this arrayList
+ * @param  list     arrayList to clear
+ * @param  clearVal Value to clear elements to
+ * @return          Whether the clear was successful
+ */
+bool arrayList_Clear(arrayList *list, float clearVal = 0);
+
+/**
+ * Frees this arrayList
+ * @param  list arrayList to free
+ * @return      Whether the free was successful
+ */
+bool arrayList_Free(arrayList *list);
 
 #endif //BCI_ARRAYLIST_H_INCLUDED
