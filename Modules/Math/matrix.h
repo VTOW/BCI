8c16d56ffc6494fe259cd53d0b2303d75e30568b
Octogonapus <firey45@gmail.com>
2016-11-08 22:56:00 -0500

    Added final functions

--- Modules/Math/matrix.h
+++ Modules/Math/matrix.h
@@ -56,6 +56,14 @@ void matrix_MultiplyByScalar(const matrix *mat, const float scalar, matrix *resu
 void matrix_DivideByScalar(const matrix *mat, const float scalar, matrix *result);
 
 /**
+ * Raises a matrix to a power
+ * @param mat    matrix to read from
+ * @param scalar Scalar
+ * @param result matrix to write result to
+ */
+void matrix_RaiseToScalar(const matrix *mat, const float scalar, matrix *result);
+
+/**
  * Adds two matricies together
  * @param mat1   First matrix
  * @param mat2   Second matrix
@@ -100,41 +108,43 @@ void matrix_Transpose(const matrix *mat, matrix *result);
 
 /**
  * Adds a scalar to every element of this matrix
- * @param mat    matrix to read from
+ * @param mat    matrix for read/write
  * @param scalar Scalar
- * @param result matrix to write result to
  */
 void matrix_AddScalar(matrix *mat, const float scalar);
 
 /**
  * Subtracts a scalar to every element of this matrix
- * @param mat    matrix to read from
+ * @param mat    matrix for read/write
  * @param scalar Scalar
- * @param result matrix to write result to
  */
 void matrix_SubtractScalar(matrix *mat, const float scalar);
 
 /**
  * Multiplies a matrix by a scalar
- * @param mat    matrix to read from
+ * @param mat    matrix for read/write
  * @param scalar Scalar
- * @param result matrix to write result to
  */
 void matrix_MultiplyByScalar(matrix *mat, const float scalar);
 
 /**
  * Divides a matrix by a scalar
- * @param mat    matrix to read from
+ * @param mat    matrix for read/write
  * @param scalar Scalar
- * @param result matrix to write result to
  */
 void matrix_DivideByScalar(matrix *mat, const float scalar);
+
+/**
+ * Raises a matrix to a power
+ * @param mat    matrix for read/write
+ * @param scalar Scalar
+ */
+void matrix_RaiseToScalar(matrix *mat, const float scalar);
 
 /**
  * Adds two matricies together
  * @param mat1   First matrix
  * @param mat2   Second matrix
- * @param result matrix to write result to
  */
 void matrix_AddMatrix(matrix *mat1, const matrix *mat2);
 
@@ -142,7 +152,6 @@ void matrix_AddMatrix(matrix *mat1, const matrix *mat2);
  * Subtracts two matricies from each other
  * @param mat1   First matrix
  * @param mat2   Second matrix
- * @param result matrix to write result to
  */
 void matrix_SubtractMatrix(matrix *mat1, const matrix *mat2);
 
@@ -150,21 +159,18 @@ void matrix_SubtractMatrix(matrix *mat1, const matrix *mat2);
  * Multiplies two matricies together
  * @param mat1   First matrix
  * @param mat2   Second matrix
- * @param result matrix to write result to
  */
 void matrix_MultiplyByMatrix(matrix *mat1, const matrix *mat2);
 
 /**
  * Inverts a matrix
- * @param mat    matrix to read from
- * @param result matrix to write to
+ * @param mat    matrix for read/write
  */
 void matrix_Invert(matrix *mat);
 
 /**
  * Transposes a matrix
- * @param mat    matrix to read from
- * @param result matrix to write to
+ * @param mat    matrix for read/write
  */
 void matrix_Transpose(matrix *mat);
 
