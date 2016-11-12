82c1946c92fd3cfec02812ff04cbe625f8a1be36
Octogonapus <firey45@gmail.com>
2016-11-12 12:16:06 -0500

    Revert "Implemented determinant"
    This reverts commit 1da288ca56950c08edcec9d143d5c6327cffa8bf.

--- Modules/Example/matrixExample.c
+++ Modules/Example/matrixExample.c
@@ -10,25 +10,17 @@ task main()
 	heap_Init();
 
   matrix m1, m2, m3;
-  matrix_Initialize(&m1, 7, 7);
-  matrix_Initialize(&m2, 7, 7);
-  matrix_Initialize(&m3, 7, 7);
+  matrix_Initialize(&m1, 3, 3);
+  matrix_Initialize(&m2, 3, 3);
+  matrix_Initialize(&m3, 3, 3);
 
-  float m1Vals[49];
-  for (int i = 0; i < 49; i++)
-  {
-  	m1Vals[i] = 1;
-  }
+  float m1Vals[9] = {1,2,3, 4,5,6, 7,8,9};
   matrix_Set(&m1, m1Vals);
 
-  float m2Vals[49];
-  for (int i = 0; i < 49; i++)
-  {
-  	m2Vals[i] = 1;
-  }
+  float m2Vals[9] = {1,2,3, 4,5,6, 7,8,9};
   matrix_Set(&m2, m2Vals);
 
-  //writeDebugStreamLine("%d",matrix_Trace(&m1));
+  writeDebugStreamLine("%d",matrix_Trace(&m1));
   //for (int i = 0; i < 3; i++)
   //{
   //	for (int j = 0; j < 3; j++)
@@ -39,17 +31,17 @@ task main()
 
   //Naive implementation takes 2889.6 milliseconds on average
   //Optimized naive implemenetation takes 872 milliseconds on average
-  for (int j = 0; j < 5; j++)
-  {
-	  long startTime, endTime;
-	  int i = 0;
-	  startTime = time1[T1];
-	  for (; i < 5; i++)
-	  {
-	  	matrix_MultiplyByMatrix(&m1, &m2, &m3);
-	  	//matrix_Transpose(&m1, &m3);
-	  }
-	  endTime = time1[T1];
-	  writeDebugStreamLine("Time taken: %d", endTime - startTime);
-	}
+ // for (int j = 0; j < 5; j++)
+ // {
+	//  long startTime, endTime;
+	//  int i = 0;
+	//  startTime = time1[T1];
+	//  for (; i < 100000; i++)
+	//  {
+	//  	//matrix_MultiplyByMatrix(&m1, &m2, &m3);
+	//  	matrix_Transpose(&m1, &m3);
+	//  }
+	//  endTime = time1[T1];
+	//  writeDebugStreamLine("Time taken: %d", endTime - startTime);
+	//}
 }
