e43cc04a403d6d832836374e870dcfff55f9c7e0
Octogonapus <firey45@gmail.com>
2017-01-14 01:04:13 -0500

    Added sp example

--- Modules/Math/statePack.c
+++ Modules/Math/statePack.c
@@ -3,18 +3,18 @@
 
 #include "statePack.h"
 
-void sp_Translate(statePack &sp, const int dist, const int angle)
+void sp_Translate(statePack *sp, const int dist, const int angle)
 {
   sp->x += cosDegrees(angle) * dist;
   sp->y += sinDegrees(angle) * dist;
 }
 
-void sp_Rotate(statePack &sp, const int angle)
+void sp_Rotate(statePack *sp, const int angle)
 {
   sp->theta += angle;
 }
 
-void sp_Rotate_Point(statePack &sp, const int angle, const long x, const long y)
+void sp_Rotate_Point(statePack *sp, const int angle, const long x, const long y)
 {
   sp->x = sp->x + (x - sp->x) * cosDegrees(angle) - (y - sp->y) * sinDegrees(angle);
   sp->y = sp->y + (x - sp->x) * sinDegrees(angle) - (y - sp->y) * cosDegrees(angle);
